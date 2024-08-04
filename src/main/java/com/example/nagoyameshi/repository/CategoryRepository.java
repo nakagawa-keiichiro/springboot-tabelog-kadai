381a943d85bacfd28b5e91325c2f8ec70f306996
nakagawa-keiichiro <volleyball2531@gmail.com>
2024-08-04 13:19:12 +0900

    カテゴリ機能追加

--- src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
+++ src/main/java/com/example/nagoyameshi/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.example.nagoyameshi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -7,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import com.example.nagoyameshi.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+	public Page<Category> findByCategoryNameLike(String keyword, Pageable pageable);
 	@Query(value ="select * from category where category_name LIKE %:keyword% ", nativeQuery = true)
 	public Category findById(@Param("keyword")String keyword);
 }
